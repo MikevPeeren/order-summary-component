79da0b2885953d83f935b8b269b7dceb86ce69b1
MikevPeeren <mikevpeeren@hotmail.com>
2021-10-06 20:42:50 +0200

    :iphone: Working on responsiveness

--- src/components/AnnualComponent.tsx
+++ src/components/AnnualComponent.tsx
@@ -4,16 +4,18 @@ import MUSIC_ICON from "../../public/icon-music.svg";
 
 const AnnualComponent = () => {
   return (
-    <div className="flex flex-row items-center w-3/4 bg-neutral-pale-blue p-5 rounded-xl">
+    <div className="flex flex-row items-center w-10/12 md:w-3/4 bg-neutral-pale-blue p-5 rounded-xl">
       <Image className="" src={MUSIC_ICON} alt="Music Icon" />
       <div className="flex flex-col px-4">
-        <span className="text-black text-lg font-black">Annual Plan</span>
-        <span className="text-neutral-desaturated-blue font-bold text-left">
+        <span className="text-black text-sm md:text-lg font-black">
+          Annual Plan
+        </span>
+        <span className="text-neutral-desaturated-blue font-bold text-sm md:text-base text-left">
           $59.99/year
         </span>
       </div>
       <a
-        className="order-2 ml-auto underline text-primary-bright-blue font-black"
+        className="text-sm md:text-base order-2 ml-auto underline text-primary-bright-blue font-black"
         href="#"
       >
         Change
--- src/components/OrderComponent.tsx
+++ src/components/OrderComponent.tsx
@@ -6,20 +6,20 @@ import AnnualComponent from "./AnnualComponent";
 
 const OrderComponent = () => {
   return (
-    <div className="h-3/4 w-1/4 bg-white flex flex-col items-center justify-between text-center rounded-2xl pb-14 shadow-lg">
+    <div className="h-3/4 md:w-2/4 lg:w-2/5 xl:w-1/4 bg-white flex flex-col items-center justify-between text-center rounded-2xl pb-14 shadow-lg mx-4">
       <Image
-        className="object-top rounded-t-2xl"
+        className="object-top rounded-t-2xl w-full"
         src={ILLUSTRATION_HERO}
         alt="Illustration of Hero"
       />
-      <h1 className="text-3xl font-black">Order Summary</h1>
-      <h2 className="w-3/4 font-medium text-neutral-desaturated-blue">
+      <h1 className="text-2xl md:text-3xl font-black">Order Summary</h1>
+      <h2 className="w-4/5 md:w-3/4 mx-4 font-medium text-neutral-desaturated-blue">
         You can now listen to millions of songs, audiobooks, and podcasts on any
         device anywhere you like!
       </h2>
       <AnnualComponent />
       <button
-        className="w-3/4 bg-primary-bright-blue hover:neutral-dark-blue text-white font-bold py-4 px-4 rounded-2xl shadow-lg"
+        className="w-10/12 md:w-3/4 bg-primary-bright-blue hover:neutral-dark-blue text-white font-bold py-4 px-4 rounded-2xl shadow-lg"
         type="button"
       >
         Proceed to Payment
